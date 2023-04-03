49a45ca035599a6fdca9a4141f0d6474197030b7
Евгений Спиридонов <deejay999@yandex.ru>
2023-04-03 12:54:22 +0300

    Revert "add routes to templates"
    This reverts commit 60b9c487

--- poll_tracker/poll_tracker/settings.py
+++ poll_tracker/poll_tracker/settings.py
@@ -59,11 +59,10 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'poll_tracker.urls'
 
-TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
