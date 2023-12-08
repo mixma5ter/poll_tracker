4eb96ed756317f868ccf67d0a141d383db0f11b5
mixmaster <deejay999@yandex.ru>
2023-12-08 15:40:22 +0300

    add photo link

--- poll_tracker/api/views.py
+++ poll_tracker/api/views.py
@@ -5,6 +5,8 @@ from rest_framework import viewsets
 from api.models import APIClient
 from core.utils import process_contest_data
 
+from poll_tracker.poll_tracker.settings import MEDIA_URL
+
 
 class ContestResultJson(viewsets.ModelViewSet):
     """Отправка данных в VMix."""
@@ -18,6 +20,11 @@ class ContestResultJson(viewsets.ModelViewSet):
         track = client.track
         stage = client.stage
         results = process_contest_data(contest, track, stage)
+        host = self.request.get_host()
+        for item in results:
+            if item['contestant__photo']:
+                item['contestant__photo'] = f'http://{host}{MEDIA_URL}{item["contestant__photo"]}'
+            item['contest'] = contest.title
         return list(results)
 
     def list(self, request, *args, **kwargs):
