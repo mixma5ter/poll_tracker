d0c737f5df6a569172b194453547f8810e7f723f
Евгений Спиридонов <deejay999@yandex.ru>
2024-02-07 13:14:04 +0300

    add app name in admin panel

--- poll_tracker/brain_ring/apps.py
+++ poll_tracker/brain_ring/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class BrainRingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'brain_ring'
+    verbose_name = 'Брейн-ринг'
