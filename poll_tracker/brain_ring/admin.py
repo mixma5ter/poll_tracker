b6f88bb745ee9cc682f855ef167b2cb632291266
Евгений Спиридонов <deejay999@yandex.ru>
2024-02-08 08:16:39 +0300

    add question filter

--- poll_tracker/brain_ring/admin.py
+++ poll_tracker/brain_ring/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 
+from contests.models import Stage
 from brain_ring.models import Question
+
+
+class QuestionStageInline(admin.TabularInline):
+    """Промежуточная модель связи между вопросами и этапами.
+
+    Модель используется для добавления фильтрации по названию этапа в админке."""
+
+    model = Stage.questions.through
+    extra = 0
+    verbose_name = 'Этап'
+    verbose_name_plural = 'Этапы'
 
 
 @admin.register(Question)
@@ -18,4 +30,10 @@ class QuestionAdmin(admin.ModelAdmin):
     )
     list_editable = ('question_index', 'is_active', 'points',)
     search_fields = ('question_index',)
-    list_filter = ('is_active',)
+    inlines = [QuestionStageInline]
+    list_filter = ('stages__title',)
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        queryset = queryset.prefetch_related('stages')
+        return queryset
